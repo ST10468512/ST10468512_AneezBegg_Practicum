7599fad291e0fa3a8df17d2b2254d4764fb8a6c4
ST10468512 <ST10468512@vcconnect.edu.za>
2025-06-19 18:23:00 +0200

    Coded complted

---
+++ app/src/main/java/za/co/varsitycollege/st10468512/st10468512aneezbeggpracticum/Details_Screen.kt
@@ -0,0 +1,96 @@
+package za.co.varsitycollege.st10468512.st10468512aneezbeggpracticum
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Details_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_details_screen)
+        // Getting the views from the layout
+        val home=findViewById<Button>(R.id.btnHome)
+        val Average=findViewById<Button>(R.id.btnAverage)
+        val Display=findViewById<Button>(R.id.btnDisplay)
+        val DisplayText=findViewById<TextView>(R.id.DisplayText)
+        val averageText=findViewById<TextView>(R.id.AverageText)
+
+
+        //Retirving the data that was passed through from the main page
+        val songsName=intent.getStringArrayListExtra("songs")?: arrayListOf()
+        val artistName=intent.getStringArrayListExtra("artist")?: arrayListOf()
+        val ratingscore=intent.getIntegerArrayListExtra("ratings")?: arrayListOf()
+        val CommentsNotes=intent.getStringArrayListExtra("comments")?: arrayListOf()
+
+
+
+
+        home.setOnClickListener {
+            //Allows the user to return to the main page
+            val intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        //creates the command of what happens once the button is clicked
+        Display.setOnClickListener {
+
+            //Checks if the songs name array is empty and display a error message if it is
+            if (songsName.isEmpty()){
+
+                DisplayText.text="Theres is no songs to display"
+                return@setOnClickListener
+            }
+
+            //Build the full playlist as a single string
+            val Displaylist =StringBuilder()
+
+
+            for(i in songsName.indices)
+            {
+
+                //writes the information to the list
+                Displaylist.append( " ${songsName[i]} -${artistName[i]} \n")
+                Displaylist.append( " Rating:${ratingscore[i]} \n")
+                Displaylist.append( " Comments: ${CommentsNotes[i] }\n  \n")
+
+                //Displays the list in the textbox
+                DisplayText.text=Displaylist.toString()
+
+            }
+        }
+
+
+        //creates the command of what happens once the button is clicked
+        Average.setOnClickListener {
+
+            var total=0
+
+            //check if the average value is empty and displays a messages
+            if (ratingscore.isEmpty()){
+                Toast.makeText(this,"There is no average",Toast.LENGTH_SHORT).show()
+                averageText.text=""
+                return@setOnClickListener
+            }
+
+            //calcualtes the average and displays it
+            for (rating in ratingscore) {
+                total += rating
+            }
+            val average = if (ratingscore.isNotEmpty()) {
+                total.toDouble() / ratingscore.size
+            }
+            else{ 0 }
+            averageText.text="Average rating is : %.2f".format(average)
+
+        }
+    }
+}
+        }
+    }
+}
--- app/src/main/java/za/co/varsitycollege/st10468512/st10468512aneezbeggpracticum/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10468512/st10468512aneezbeggpracticum/MainActivity.kt
@@ -1,17 +1,120 @@
 package za.co.varsitycollege.st10468512.st10468512aneezbeggpracticum
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
+
+        //Arrays that will be used to store the song information
+        var songNames = ArrayList<String>()
+
+        var artistArray = ArrayList<String>()
+
+        var ratingArray = ArrayList<Int>()
+
+        var commentsArray = ArrayList<String>()
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContentView(R.layout.activity_main)
+
+            //Getting the buttons from the layout
+            val ExitButton = findViewById<Button>(R.id.btnExit)
+            val NextPage = findViewById<Button>(R.id.btnNext)
+            val AddToPlaylist = findViewById<Button>(R.id.btnAdd)
+
+
+            //Creating the exit button so that once its clicked the program will close
+            ExitButton.setOnClickListener {
+                finishAffinity()
+            }
+
+
+            //Creating the command whaat will happen once the  button is clicked
+            AddToPlaylist.setOnClickListener {
+
+                //Showing the dialog in order to get the song information
+                AddSongs()
+
+
+            }
+
+            //Creating the command of what will happe once the next page button is clicked
+            NextPage.setOnClickListener {
+
+                //Using intent to pass the user to the next page
+                val intent = Intent(this, Details_Screen::class.java)
+
+                //Using intent to pass the information stored in the arrays to the next screen so that it can be accessed on the next screen
+                intent.putStringArrayListExtra("songs",songNames)
+                intent.putStringArrayListExtra("artist",artistArray)
+                intent.putIntegerArrayListExtra("ratings",ratingArray)
+                intent.putStringArrayListExtra("comments",commentsArray)
+
+                //starting the next page
+                startActivity(intent)
+
+            }
+        }
+
+        //declaring a private function that will be used
+        private fun AddSongs() {
+            val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_list, null)
+
+            //Getting the textboxes from the dialog page
+            val SongInput = dialogView.findViewById<EditText>(R.id.EditTextSong)
+            val ArtistName = dialogView.findViewById<EditText>(R.id.editTextArtist)
+            val RatingInput = dialogView.findViewById<EditText>(R.id.editTextRating)
+            val CommentsInput = dialogView.findViewById<EditText>(R.id.editTextComments)
+
+
+            //Creating and showing the dialog box
+            val dialog = AlertDialog.Builder(this)
+                .setTitle("Add Song Name")
+                .setView(dialogView)
+                .setPositiveButton("Add")
+                { _, _ ->
+
+                    // Retriving the information from the dialog boxes
+                    val song = SongInput.text.toString()
+                    val artist = ArtistName.text.toString()
+                    val Rating = RatingInput.text.toString().toIntOrNull()
+                    val Comments = CommentsInput.text.toString()
+
+                    //Adding to the arrays if all the field are valid
+                    if (song.isNotEmpty() && artist.isNotEmpty() && Rating != null && Rating in 1..5) {
+                        songNames.add(song)
+                        artistArray.add(artist)
+                        ratingArray.add(Rating)
+                        commentsArray.add(Comments)
+
+                        //showing a confirmation message
+                        Toast.makeText(this, "Song Added", Toast.LENGTH_SHORT).show()
+                    } else {
+
+                        //Showing a error message
+                        Toast.makeText(this, "Please fill all the field correctly", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                //creating the cancel button on the dialog
+                .setNegativeButton("Cancel", null)
+                .show()
+                .create()
+
+
+        }
+    }
         }
     }
 }
